bd8d524c43e2e59b34292d4679915fab5a31248a
amir <ladsgroup@gmail.com>
2015-08-21 15:45:53 +0430

    Fix for process_item

--- wb_vandalism/datasources/parsed_revision_text.py
+++ wb_vandalism/datasources/parsed_revision_text.py
@@ -7,6 +7,7 @@ from revscoring.datasources.revision import text
 def process_item(text):
     item = pywikibase.ItemPage()
     item.get(content=json.loads(text))
+    return item
 
 
 item = Datasource("item", process_item, depends_on=[text])
