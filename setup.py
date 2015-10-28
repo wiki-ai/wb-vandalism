95ea07a6ae1dd44aec369b78c4cda7f2c2585bf1
halfak <aaron.halfaker@gmail.com>
2015-10-28 11:13:47 -0500

    Increments version to 0.1.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def requirements(fname):
     for line in open(os.path.join(os.path.dirname(__file__), fname)):
         yield line.strip()
 
-version = "0.1.0"
+version = "0.1.1"
 
 setup(
     name="wb_vandalism",
