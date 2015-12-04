10755960e11f1516bdd402b43a2e45e5e65f100a
amir <ladsgroup@gmail.com>
2015-12-05 02:49:28 +0330

    Release 0.1.6

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def requirements(fname):
     for line in open(os.path.join(os.path.dirname(__file__), fname)):
         yield line.strip()
 
-version = "0.1.4"
+version = "0.1.6"
 
 setup(
     name="wb_vandalism",
